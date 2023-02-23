b705d4d4c84eee55be3f90905577cf35593642aa
Kennedy Brito <brito.kennedy10@gmail.com>
2023-02-23 11:15:11 -0300

    endpoint findAll tecnicos

--- src/main/java/com/helpdesk/services/TecnicoService.java
+++ src/main/java/com/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,10 @@ public class TecnicoService {
 		return obj.orElseThrow(() -> new ObjectnotFoundException("objeto não encontrado ID:" + id));
 		
 	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
 	
-	//(() -> new ObjectnotFoundException("objeto não encontrado ID:" + id));
 
 }
